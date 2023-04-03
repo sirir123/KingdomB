7ad1513defcc93f98ba3404ee10f3994b847909f
siri <siri@ramineni.name>
2023-04-03 07:51:02 -0500

    hex tilehex outlines

--- TileHex.java
+++ TileHex.java
@@ -1,3 +1,34 @@
 public class TileHex extends Hex{
-    
+    private int amount, status;
+
+    public TileHex(String t, int c, int r, int s){
+        super(t, c, r, s);
+        amount = 2;
+        setpNum(-1);
+        status = 0
+    }
+
+    public void statStill(){ 
+        status = 0;
+    }
+
+    public void statUnused(){
+        status = 1;
+    }
+
+    public void statUsed(){ 
+        status = 2;
+    }
+
+    public void statDiscard(){ 
+        status = 3;
+    }
+
+    public int getStat(){ 
+        return status;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
 }
