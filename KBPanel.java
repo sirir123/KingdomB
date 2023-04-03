0eb92c755935f3e435b2ae487d4da3d0e5c22d93
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2023-04-03 08:42:54 -0500

    grsphics

--- KBPanel.java
+++ KBPanel.java
@@ -12,9 +12,10 @@ import java.util.*;
 public class KBPanel extends JPanel implements MouseListener {
     private BufferedImage startScr, plyPick;
     private int numPly;
-    private boolean start;
+    private boolean start, end;
 
     public KBPanel() {
+        numPly = 2;
         start = false;
         try {
             startScr = ImageIO.read(KBPanel.class.getResource("/Pictures/startScreen.png"));
@@ -44,15 +45,18 @@ public class KBPanel extends JPanel implements MouseListener {
 
         System.out.println("loc is(" + x + ", " + y + ")");
         if (!start && y >= 569 && y <= 624) {
-            if (x >= 654 && x <= 791) { // for start screen
+            if (x >= 654 && x <= 690) { // for start screen
+                System.out.println("2");
                 numPly = 2;
             } else if (x >= 744 && x <= 782) {
-
-            } else if (x >= 833 && x < 869) {
-
+                System.out.println("3");
+                numPly = 3;
+            } else if (x >= 833 && x <= 869) {
+                System.out.println("4");
+                numPly = 4;
             }
         }
-
+        repaint();
     }
 
     public void addNotify() { // tbh idk what this does I just always have it and am paranoid its not working
@@ -62,7 +66,42 @@ public class KBPanel extends JPanel implements MouseListener {
     }
 
     public void paint(Graphics g) {
+        if (!start) {
+            drawStartScreen(g);
+        }
+        if (end) {
+
+        } else { // rest of game
+
+        }
+
+    }
+
+    public void drawStartScreen(Graphics g) {
         g.drawImage(startScr, 0, 0, getWidth(), getHeight(), null);
-        g.drawImage(plyPick, 654, 569, 37, 55, null);
+        // shows how many players choosen
+        switch (numPly) {
+            case 2:
+                g.drawImage(plyPick, 654, 569, 37, 55, null);
+                break;
+            case 3:
+                g.drawImage(plyPick, 744, 569, 37, 55, null);
+                break;
+            case 4:
+                g.drawImage(plyPick, 833, 569, 37, 55, null);
+                break;
+        }
+    }
+
+    public void drawPlayers(Graphics g) {
+
+    }
+
+    public void drawBoard(Graphics g) {
+
+    }
+
+    public void drawSettlements(Graphics g) {
+
     }
 }
