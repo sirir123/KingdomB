c0362653b5a1ef505ccd5733f33da3c7e24e614a
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2023-04-03 07:52:11 -0500

    new

--- KBPanel.java
+++ KBPanel.java
@@ -10,11 +10,15 @@ import java.awt.event.MouseEvent;
 import java.util.*;
 
 public class KBPanel extends JPanel implements MouseListener {
-    private BufferedImage start;
+    private BufferedImage startScr, plyPick;
+    private int numPly;
+    private boolean start;
 
     public KBPanel() {
+        start = false;
         try {
-
+            startScr = ImageIO.read(KBPanel.class.getResource("/Pictures/startScreen.png"));
+            plyPick = ImageIO.read(KBPanel.class.getResource("/Pictures/playerPick.png"));
         } catch (Exception E) {
             System.out.println("Exception Error");
             return;
@@ -39,6 +43,16 @@ public class KBPanel extends JPanel implements MouseListener {
         int y = e.getY();
 
         System.out.println("loc is(" + x + ", " + y + ")");
+        if (!start && y >= 569 && y <= 624) {
+            if (x >= 654 && x <= 791) { // for start screen
+                numPly = 2;
+            } else if (x >= 744 && x <= 782) {
+
+            } else if (x >= 833 && x < 869) {
+
+            }
+        }
+
     }
 
     public void addNotify() { // tbh idk what this does I just always have it and am paranoid its not working
@@ -48,5 +62,7 @@ public class KBPanel extends JPanel implements MouseListener {
     }
 
     public void paint(Graphics g) {
+        g.drawImage(startScr, 0, 0, getWidth(), getHeight(), null);
+        g.drawImage(plyPick, 654, 569, 37, 55, null);
     }
 }
