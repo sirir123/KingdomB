b61eba28a7390a2c863bcf2bcc8faa3e204907e7
sophia <sophia.l18492@gmail.com>
2023-04-04 07:48:19 -0500

    player and card classes started

--- Player.java
+++ Player.java
@@ -1,3 +1,20 @@
 public class Player{
+    private int settlements=40;
+    private int num;
+    private String color;
+    private Card chosen;
+    private String type;
+    private ArrayList<TileHex>() tiles;
+
+    public int getSett(){
+        return settlements;
+    }
+    public void setChosen(Card c){
+        chosen=c;
+    }
+    public ArrayList<TileHex>() getTiles(){
+        return tiles;
+    }
     
+
 }
