810d766da7e0b92c67e2846163b7f3a2ed360a4f
sophia <sophia.l18492@gmail.com>
2023-04-05 07:49:45 -0500

    basic layout for scorecard class

--- ScoreCard.java
+++ ScoreCard.java
@@ -2,7 +2,8 @@ public class ScoreCard{
     private String type;
 
     public ScoreCard(){
-        
+        type="lord, workers, discoverers";
+        //doesn't matter much now bc we're only using 3 scoring cards anyways
     }
 
     public int score(Player p){
@@ -15,18 +16,18 @@ public class ScoreCard{
     }
 
     public int lords(Player p){
-
+        //check each section for top 2 amount of settlements
     }
 
     public int workers(Player p){
-
+        //gain 1 gold for a settlement next to location or castle hex
     }
 
     public int discoverers(Player p){
-
+        //gain 1 gold for every settlement on horizontal line
     }
 
     public int castle(Player p){
-
+        //3 gold per castle hex
     }
 }
