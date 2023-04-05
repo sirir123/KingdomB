8080785ab4ba6a33291aaf0cb383536e8f974ad2
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2023-04-05 07:29:29 -0500

    commie

--- Card.java
+++ Card.java
@@ -1,11 +1,12 @@
-public class Card{
-    private String terr="";
+public class Card {
+    private String terr = ""; // grass, canyon, sand, flower, forest
 
-    public String getTerr(){
+    public String getTerr() {
         return terr;
     }
-    public void setTerr(String change){
-        terr=change;
+
+    public void setTerr(String change) {
+        terr = change;
     }
-    
+
 }
