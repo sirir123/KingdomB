f31efa7027b547a591a6abe7bcd35d63141b9eaa
sophia <sophia.l18492@gmail.com>
2023-04-05 07:41:44 -0500

    started basic layout for scorecard class

--- ScoreCard.java
+++ ScoreCard.java
@@ -1,3 +1,32 @@
 public class ScoreCard{
-    
+    private String type;
+
+    public ScoreCard(){
+        
+    }
+
+    public int score(Player p){
+        int gold=0;
+        gold+=lords(p);
+        gold+=workers(p);
+        gold+=discoverers(p);
+        gold+=castle(p);
+        return gold;
+    }
+
+    public int lords(Player p){
+
+    }
+
+    public int workers(Player p){
+
+    }
+
+    public int discoverers(Player p){
+
+    }
+
+    public int castle(Player p){
+
+    }
 }
