92c90cf809b458664b44674e0c9764c2557c651a
sophia <sophia.l18492@gmail.com>
2023-04-05 07:27:09 -0500

    updated player class again

--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ public class Player{
     private int settlements=40;
     private int num;
     private String color;
-    private String type;// or Card chosen?
+    private String type;
     private ArrayList<TileHex> tiles= new ArrayList<TileHex>();
     private ArrayList<Integer> points= new ArrayList<Integer>();
 
@@ -10,8 +10,12 @@ public class Player{
         return settlements;
     }
 
-    public ArrayList<TileHex> getTiles(){
+    public ArrayList<TileHex> getAllTiles(){
         return tiles;
+    }
+
+    public TileHex getTile(int num){
+        return tiles.get(num);
     }
 
     public void addTile(TileHex t){
@@ -20,6 +24,14 @@ public class Player{
 
     public void addPoints(int num){
         points.add(num);
+    }
+
+    public ArrayList<Integer> getAllPoints(){
+        return points;
+    }
+
+    public Integer getPoint(int num){
+        return points.get(num);
     }
 
     public void setChosen(Card c){
