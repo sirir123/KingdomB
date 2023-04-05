fd57f87ca08daa99a535b6a996ca28fe5ffd86d9
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2023-04-05 07:29:51 -0500

    load

--- Game.java
+++ Game.java
@@ -1,3 +1,12 @@
-public class Game{
-    
+public class Game {
+    private int numPly;
+
+    public Game() {
+        numPly = 2;
+    }
+
+    public void setNumPly(int n) {
+        numPly = n;
+    }
+
 }
